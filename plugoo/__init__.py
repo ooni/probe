70def2b8642b3b0f2c9a697f5bcadf7e99a18981
Arturo Filastò <hellais@gmail.com>
2012-03-07 21:04:02 -0800

    Add a list of things TODO

--- plugoo/__init__.py
+++ plugoo/__init__.py
@@ -27,6 +27,33 @@ import logging
 import itertools
 import gevent
 
+def gen_headers(self, options="common"):
+    """
+    Returns a set of headers to be used when generating
+    HTTP requests.
+
+    :options specify what rules should be used for
+             generating the headers.
+             "common": choose a very common header set (default)
+             "random": make the headers random
+    """
+    if options == "common":
+        headers = [('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+         ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'),
+         ('Accept-Encoding', 'gzip,deflate,sdch'),
+         ('Accept-Language', 'en,en-US;q=0.8,it;q=0.6'),
+         ('Cache-Control', 'max-age=0')
+         ('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11')]
+    elif options == "random":
+        # XXX not implemented
+        return False
+    else:
+        print "Error, unrecognized header generation options.."
+        return False
+
+    return headers
+
+
 class torify(object):
     """This is the torify decorator. It should be used to
     decorate functions that should use to for connecting to
