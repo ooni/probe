29cc044b50cf0fdc8bfad97f194ea7fa993e08e6
Arturo Filastò <hellais@torproject.org>
2012-05-31 15:38:48 +0200

    Write test case for worker

---
+++ tests/test_worker.py
@@ -0,0 +1,18 @@
+from twisted.trial import unittest
+
+from ooni.plugoo import work, tests
+
+class WorkerTestCase(unittest.TestCase):
+    def testWorkGenerator(self):
+        class DummyTest:
+            assets = {}
+        dummytest = DummyTest()
+        asset = []
+        for i in range(10):
+            asset.append(i)
+        dummytest.assets['asset'] = asset
+        wgen = work.WorkGenerator(dummytest)
+        for j, x in enumerate(wgen):
+            pass
+        self.assertEqual(i, j)
+
