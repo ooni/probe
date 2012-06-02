a6eebf219c45431721071baa2b295bf1bd1c77fd
Arturo Filastò <hellais@torproject.org>
2012-06-02 21:10:15 +0200

    Remove dead code.

--- ooni/plugoo/__init__.py
+++ ooni/plugoo/__init__.py
@@ -17,15 +17,8 @@ import os
 from datetime import datetime
 import yaml
 
-try:
-    import socks
-except:
-    "Error SocksiPy is not installed!"
-import socket
-
 import logging
 import itertools
-import gevent
 
 def gen_headers(self, options="common"):
     """
@@ -52,29 +45,3 @@ def gen_headers(self, options="common"):
         return False
 
     return headers
-
-
-class torify(object):
-    """This is the torify decorator. It should be used to
-    decorate functions that should use to for connecting to
-    the interwebz. The suggary syntax is the following:
-    @torify([urllib2])
-    def myfunction():
-        f = urllib2.urlopen('https://torproject.org/')
-    remember to set the proxyaddress in the config file.
-    """
-    def __init__(self, f):
-        print f
-        self.f = f
-
-    def __get__(self, instance, owner):
-        self.modules = instance.modules
-        def decorator(*args):
-            print instance.config.main.proxyaddress
-            host, port = instance.config.main.proxyaddress.split(":")
-            socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, host, int(port))
-            # Wrap the modules into socks
-            for module in self.modules:
-                socks.wrapmodule(module)
-            return self.f(instance, *args)
-        return decorator
