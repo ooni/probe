654be17e244713320f6d218f447de1a62546c6b3
Arturo Filastò <art@baculo.org>
2012-06-14 01:24:36 +0200

    Add scapy protocol and example for it * Add example of writing a tcp connect scanner

---
+++ ooni/plugins/tcpconnect.py
@@ -0,0 +1,62 @@
+"""
+This is a self genrated test created by scaffolding.py.
+you will need to fill it up with all your necessities.
+Safe hacking :).
+"""
+from zope.interface import implements
+from twisted.python import usage
+from twisted.plugin import IPlugin
+from twisted.internet import reactor
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint
+
+from ooni.plugoo.tests import ITest, OONITest
+from ooni.plugoo.assets import Asset
+from ooni import log
+
+class tcpconnectArgs(usage.Options):
+    optParameters = [['blabla', 'a', None, 'Asset file'],
+                     ['resume', 'r', 0, 'Resume at this index']]
+
+class tcpconnectTest(OONITest):
+    implements(IPlugin, ITest)
+
+    shortName = "tcpconnect"
+    description = "tcpconnect"
+    requirements = None
+    options = tcpconnectArgs
+    blocking = False
+
+    def experiment(self, args):
+        host, port = args['blabla'].split(':')
+        class DummyFactory(Factory):
+            def buildProtocol(self, addr):
+                return Protocol()
+
+        def gotProtocol(p):
+            p.transport.loseConnection()
+            log.msg("Got a connection!")
+            log.msg(str(p))
+            return {'result': True, 'target': [host, port]}
+
+        def gotError(err):
+            log.msg("Had error :(")
+            log.msg(err)
+            return {'result': False, 'target': [host, port]}
+
+        # What you return here gets handed as input to control
+        point = TCP4ClientEndpoint(reactor, host, int(port))
+        d = point.connect(DummyFactory())
+        d.addCallback(gotProtocol)
+        d.addErrback(gotError)
+        return d
+
+    def load_assets(self):
+        if self.local_options:
+            return {'blabla': Asset(self.local_options['blabla'])}
+        else:
+            return {}
+
+# We need to instantiate it otherwise getPlugins does not detect it
+# XXX Find a way to load plugins without instantiating them.
+tcpconnect = tcpconnectTest(None, None, None)
