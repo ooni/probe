3e85b9e1e589c08889c4607b9597775a6144e465
Arturo Filastò <art@baculo.org>
2012-06-15 23:30:39 +0200

    Refactor test tester

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,3 +1,4 @@
+from twisted.internet import defer
 from twisted.trial import unittest
 
 from ooni.plugoo import work, tests
@@ -12,11 +13,15 @@ class TestsTestCase(unittest.TestCase):
         self.errbackResults = None
 
     def _callback(self, *args, **kw):
+        print "BBB"
+        print args, kw
+        print "CCCC"
         self.callbackResults = args, kw
 
     def _errback(self, *args, **kw):
         pass
 
+    @defer.inlineCallbacks
     def test_fallThrough(self):
         """
         This tests to make sure that what is returned from the experiment
@@ -24,10 +29,16 @@ class TestsTestCase(unittest.TestCase):
         """
         test_dict = {"hello": "world"}
         class DummyTest(tests.OONITest):
+            blocking = False
             def experiment(self, args):
-                return test_dict
+                def cb(aaa):
+                    return test_dict
+                d = defer.Deferred()
+                d.addCallback(cb)
+                d.callback(None)
+                return d
 
         test = DummyTest(None, None, self.dummyreport)
-        test.startTest(None).addCallback(self._callback)
-        self.assertEqual(self.callbackResults[0][0]['result'], test_dict)
+        yield test.startTest(None).addCallback(self._callback)
+        self.assertEqual(self.callbackResults[0][0]['control'], test_dict)
 
