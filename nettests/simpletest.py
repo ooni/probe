3172aff3c1cc90a5e88d60d362cb73336b0dc646
Arturo Filastò <arturo@filasto.net>
2012-10-23 18:09:57 +0000

    Add a very simple test that *must* always pass. * Useful for testing the newstyle API

---
+++ nettests/simpletest.py
@@ -0,0 +1,19 @@
+from ooni import nettest
+class SimpleTest(nettest.TestCase):
+    inputs = range(1,100)
+    optParameters = [['asset', 'a', None, 'Asset file'],
+                     ['controlserver', 'c', 'google.com', 'Specify the control server'],
+                     ['resume', 'r', 0, 'Resume at this index'],
+                     ['other', 'o', None, 'Other arguments']]
+
+    def test_foo(self, *arg, **kw):
+        print "Running %s with %s" % ("test_foo", self.input)
+        self.report['test_foo'] = 'Antani'
+        self.report['shared'] = "sblinda"
+        self.assertEqual(1,1)
+
+    def test_f4oo(self, *arg, **kw):
+        print "Running %s with %s" % ("test_f4oo", self.input)
+        self.report['test_f4oo'] = 'Antani'
+        self.report['shared'] = "sblinda2"
+        self.assertEqual(1,1)
