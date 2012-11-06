e6ac93347a50096897de673a368a99c557f5cef7
Arturo Filastò <arturo@filasto.net>
2012-11-06 15:51:29 +0100

    Refactor log file rename configuration to oconfig

---
+++ ooni/oconfig.py
@@ -0,0 +1,7 @@
+from ooni.utils import Storage
+
+# XXX move this to an actual configuration file
+basic = Storage()
+advanced = Storage()
+
+advanced.debug = True
--- ooni/utils/log.py
+++ ooni/utils/log.py
@@ -11,22 +11,21 @@ from twisted.python import log as txlog
 from twisted.python.logfile import DailyLogFile
 
 from ooni.utils import otime
-## error on import with new __init__ file: 31/10/12
-from ooni import config
+from ooni import oconfig
 
 # XXX make this a config option
-log_file = "/tmp/ooniprobe.log"
+log_file = oconfig.basic.logfile
 
-log_folder = os.path.join('/', *log_file.split('/')[:-1])
-log_filename = log_file.split('/')[-1]
-daily_logfile = DailyLogFile(log_filename, log_folder)
+def start(log_file=log_file):
+    log_folder = os.path.join('/', *log_file.split('/')[:-1])
+    log_filename = log_file.split('/')[-1]
+    daily_logfile = DailyLogFile(log_filename, log_folder)
 
-def start():
     txlog.msg("Starting OONI on %s (%s UTC)" %  (otime.prettyDateNow(),
                                                  otime.utcPrettyDateNow()))
     logging.basicConfig()
     python_logging = txlog.PythonLoggingObserver()
-    if config.advanced.debug:
+    if oconfig.advanced.debug:
         python_logging.logger.setLevel(logging.DEBUG)
     else:
         python_logging.logger.setLevel(logging.INFO)
@@ -49,3 +48,4 @@ def err(msg, *arg, **kw):
 
 def exception(*msg):
     logging.exception(msg)
+
