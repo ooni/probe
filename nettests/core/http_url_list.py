5944c3c16c16df0c5a6103193b0753b2456a6360
Arturo Filastò <art@fuffa.org>
2012-11-20 17:40:13 +0100

    Rename url list test to HTTP URL List * Refactor of the test logic

--- nettests/core/http_url_list.py
+++ nettests/core/http_url_list.py
@@ -3,6 +3,7 @@
 # :authors: Arturo Filastò
 # :licence: see LICENSE
 
+from twisted.internet import defer
 from twisted.python import usage
 from ooni.templates import httpt
 
@@ -12,15 +13,15 @@ class UsageOptions(usage.Options):
                      ['url', 'u', None, 'Specify a single URL to test.']
                     ]
 
-class URLList(httpt.HTTPTest):
+class HTTPURLList(httpt.HTTPTest):
     """
     Performs GET, POST and PUT requests to a list of URLs specified as
     input and checks if the page that we get back as a result matches that
     which we expect.
     """
-    name = "URL List"
+    name = "HTTP URL List"
     author = "Arturo Filastò"
-    version = "0.1.2"
+    version = "0.1.3"
 
     usageOptions = UsageOptions
 
@@ -38,7 +39,7 @@ class URLList(httpt.HTTPTest):
         else:
             raise Exception("No input specified")
 
-    def check_for_censorship(self, body):
+    def check_for_content_censorship(self, body):
         """
         If we have specified what a censorship page looks like here we will
         check if the page we are looking at matches it.
@@ -46,18 +47,21 @@ class URLList(httpt.HTTPTest):
         XXX this is not tested, though it is basically what was used to detect
         censorship in the palestine case.
         """
+        self.report['censored'] = True
+
+        censorship_page = open(self.localOptions['content'])
+        response_page = iter(body.split("\n"))
+
+        for censorship_line in censorship_page.xreadlines():
+            response_line = response_page.next()
+            if response_line != censorship_line:
+                self.report['censored'] = False
+                break
+        censorship_page.close()
+
+    def processResponseBody(self, body):
         if self.localOptions['content']:
-            self.report['censored'] = True
-
-            censorship_page = open(self.localOptions['content'])
-            response_page = iter(body.split("\n"))
-
-            for censorship_line in censorship_page.xreadlines():
-                response_line = response_page.next()
-                if response_line != censorship_line:
-                    self.report['censored'] = False
-                    break
-            censorship_page.close()
+            self.check_for_content_censorship(body)
 
     def test_get(self):
         return self.doRequest(self.url, method="GET")
