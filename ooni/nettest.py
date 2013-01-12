7d98b7c626d9647b4b52fe7306ffe0968b762da4
Arturo Filastò <art@fuffa.org>
2013-01-12 14:56:48 +0100

    Re-organize module structure

--- ooni/nettest.py
+++ ooni/nettest.py
@@ -10,6 +10,67 @@ from twisted.python import usage
 from ooni.errors import handleAllFailures, failureToString
 from ooni.utils import log
 
+class NetTest(object):
+    director = None
+
+    def __init__(self, net_test_file, inputs, options, report):
+        """
+        The NetTest object is responsible for keeping a reference to the
+        Report and the loading of NetTestCases from test files.
+
+        net_test_file:
+            is a file object containing the test to be run.
+
+        inputs:
+            is a generator containing the inputs to the net test.
+
+        options:
+            is a dict containing the opitions to be passed to the net test.
+        """
+        self.test_cases = self.loadNetTestFile(net_test_file)
+        self.inputs = inputs
+        self.options = options
+
+    def loadNetTestFile(self, net_test_file):
+        """
+        Creates all the necessary test_cases (a list of tuples containing the
+        NetTestCase (test_class, test_method))
+
+        example:
+            [(test_classA, test_method1),
+            (test_classA, test_method2),
+            (test_classA, test_method3),
+            (test_classA, test_method4),
+            (test_classA, test_method5),
+
+            (test_classB, test_method1),
+            (test_classB, test_method2)]
+
+        Note: the inputs must be valid for test_classA and test_classB.
+
+        net_test_file:
+            is a file like object that will be used to generate the test_cases.
+        """
+        # XXX Not implemented
+        raise NotImplemented
+
+    def succeeded(self, measurement):
+        """
+        This gets called when a measurement has failed.
+        """
+        self.report.write(measurement)
+
+    def generateMeasurements(self):
+        """
+        This is a generator that yields measurements and sets their timeout
+        value and their netTest attribute.
+        """
+        for test_input in self.inputs:
+            for test_class, test_method in self.test_cases:
+                measurement = Measurement(test_class, test_method, test_input)
+                measurement.netTest = self
+                measurement.timeout = self.rateLimiter.timeout
+                yield measurement
 
 class NoPostProcessor(Exception):
     pass
