e62e945111812cda07a33b934478ec8fe0271912
aagbsn <aagbsn@extc.org>
2013-01-15 12:31:24 +0000

    Move Reporter Mocks into mocks.py

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -6,38 +6,10 @@ from ooni.managers import ReportEntryManager, TaskManager
 from ooni.nettest import NetTest
 
 from ooni.tasks import TaskMediator, TaskWithTimeout
+from tests.mocks import MockOReporter, MockTaskManager
+from tests.mocks import MockMeasurement, MockNetTest
 
 mockReportOptions = {'name':'foo_test', 'version': '0.1'}
-
-class MockOReporter(object):
-    def __init__(self):
-        self.created = defer.Deferred()
-
-    def writeReportEntry(self, entry):
-        pass
-
-    def finish(self):
-        pass
-
-    def createReport(self):
-        pass
-
-class MockMeasurement(TaskWithTimeout):
-    def __init__(self):
-        TaskWithTimeout.__init__(self)
-
-    def succeeded(self, result):
-        pass
-
-class MockTaskManager(TaskManager):
-    def __init__(self):
-        self.successes = []
-
-    def failed(self, failure, task):
-        pass
-
-    def succeeded(self, result, task):
-        self.successes.append((result, task))
 
 class TestReport(unittest.TestCase):
     def setUp(self):
@@ -46,7 +18,7 @@ class TestReport(unittest.TestCase):
 
     def test_report_alltasksdone_callback_fires(self):
         for m in range(10):
-            measurement = MockMeasurement()
+            measurement = MockMeasurement(MockNetTest())
             self.report.write(measurement)
 
         self.report.report_mediator.allTasksScheduled()
