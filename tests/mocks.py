fca37fd41c1db6f1bf7e0866eb4e7168dc6b923f
aagbsn <aagbsn@extc.org>
2013-01-15 12:23:28 +0000

    Move Managers Mocks into mocks.py

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,14 +1,8 @@
-from ooni.tasks import BaseTask
+from ooni.tasks import BaseTask, TaskWithTimeout
+from twisted.python import failure
 from ooni.nettest import NetTest
 from ooni.managers import TaskManager
-
-class MockMeasurement(BaseTask):
-    def run(self):
-        f = open('foo.txt', 'w')
-        f.write('testing')
-        f.close()
-
-        return defer.succeed(self)
+from twisted.internet import defer
 
 class MockMeasurementFailOnce(BaseTask):
     def run(self):
@@ -19,18 +13,6 @@ class MockMeasurementFailOnce(BaseTask):
             return defer.succeed()
         else:
             return defer.fail()
-
-class MockNetTest(NetTest):
-    def __init__(self, num_measurements=1):
-        NetTest.__init__(self, StringIO(net_test_string), dummyOptions)
-        self.num_measurements = num_measurements
-    def generateMeasurements(self):
-        for i in range(self.num_measurements):
-            yield MockMeasurement()
-
-class MockDirector(object):
-    def __init__(self):
-        pass
 
 class MockMeasurementManager(TaskManager):
     def __init__(self):
@@ -46,4 +28,89 @@ class MockReporter(object):
     def write(self, measurement):
         pass
 
+## from test_managers
+mockFailure = failure.Failure(Exception('mock'))
 
+class MockSuccessTask(BaseTask):
+    def run(self):
+        return defer.succeed(42)
+
+class MockFailTask(BaseTask):
+    def run(self):
+        return defer.fail(mockFailure)
+
+class MockFailOnceTask(BaseTask):
+    def run(self):
+        if self.failures >= 1:
+            return defer.succeed(42)
+        else:
+            return defer.fail(mockFailure)
+
+class MockSuccessTaskWithTimeout(TaskWithTimeout):
+    def run(self):
+        return defer.succeed(42)
+
+class MockFailTaskThatTimesOut(TaskWithTimeout):
+    def run(self):
+        return defer.Deferred()
+
+class MockTimeoutOnceTask(TaskWithTimeout):
+    def run(self):
+        if self.failures >= 1:
+            return defer.succeed(42)
+        else:
+            return defer.Deferred()
+
+class MockFailTaskWithTimeout(TaskWithTimeout):
+    def run(self):
+        return defer.fail(mockFailure)
+
+class MockTaskManager(TaskManager):
+    def __init__(self):
+        self.successes = []
+
+    def failed(self, failure, task):
+        pass
+
+    def succeeded(self, result, task):
+        self.successes.append((result, task))
+
+class MockNetTest(object):
+    def __init__(self):
+        self.successes = []
+
+    def succeeded(self, measurement):
+        self.successes.append(measurement)
+
+class MockMeasurement(TaskWithTimeout):
+    def __init__(self, net_test):
+        TaskWithTimeout.__init__(self)
+        self.netTest = net_test
+
+    def succeeded(self, result):
+        return self.netTest.succeeded(self)
+
+class MockSuccessMeasurement(MockMeasurement):
+    def run(self):
+        return defer.succeed(42)
+
+class MockFailMeasurement(MockMeasurement):
+    def run(self):
+        return defer.fail(mockFailure)
+
+class MockFailOnceMeasurement(MockMeasurement):
+    def run(self):
+        if self.failures >= 1:
+            return defer.succeed(42)
+        else:
+            return defer.fail(mockFailure)
+
+class MockDirector(object):
+    def __init__(self):
+        self.successes = []
+
+    def measurementFailed(self, failure, measurement):
+        pass
+
+    def measurementSucceeded(self, measurement):
+        self.successes.append(measurement)
