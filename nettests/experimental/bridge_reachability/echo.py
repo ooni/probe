d613ca0e63d4b3cb09fd1c6491fb328b6b290875
Isis Lovecruft <isis@torproject.org>
2013-01-30 03:13:56 +0000

    Working copy of echo test.

--- nettests/experimental/bridge_reachability/echo.py
+++ nettests/experimental/bridge_reachability/echo.py
@@ -7,126 +7,147 @@
 #     A simple ICMP-8 ping test.
 #
 # @authors: Isis Lovecruft, <isis@torproject.org>
-# @version: 0.0.2-pre-alpha
 # @license: copyright (c) 2012 Isis Lovecruft
 #           see attached LICENCE file
-#
 
 import os
-import sys
 
+from twisted.internet import defer
 from twisted.python   import usage
-from twisted.internet import reactor, defer
-from ooni             import nettest
-from ooni.utils       import log, net, Storage, txscapy
+from ooni             import nettest, config
+from ooni.utils       import log, net, randomStr
 
 try:
-    from scapy.all             import IP, ICMP
-    from scapy.all             import sr1
-    from ooni.lib              import txscapy
-    from ooni.lib.txscapy      import txsr, txsend
-    from ooni.templates.scapyt import BaseScapyTest
-except:
+    from scapy.all      import IP, ICMP, sr1
+    from ooni.utils     import txscapy
+except Exception, e:
     log.msg("This test requires scapy, see www.secdev.org/projects/scapy")
+    log.exception(e)
 
-class UsageOptions(usage.Options):
+class EchoOptions(usage.Options):
+    """
+    Options for EchoTest.
+    """
     optParameters = [
         ['dst', 'd', None, 'Host IP to ping'],
-        ['file', 'f', None, 'File of list of IPs to ping'],
-        ['interface', 'i', None, 'Network interface to use'],
+        ['receive', 'r', True, 'Receive response packets'],
         ['count', 'c', 1, 'Number of packets to send', int],
-        ['size', 's', 56, 'Number of bytes to send in ICMP data field', int],
-        ['ttl', 'l', 25, 'Set the IP Time to Live', int],
-        ['timeout', 't', 2, 'Seconds until timeout if no response', int],
-        ['pcap', 'p', None, 'Save pcap to this file'],
-        ['receive', 'r', True, 'Receive response packets']]
+        ['icmp_type', 'i', 8, 'ICMP-type to send', int],
+        ['payload_size', 'p', 56, 'Bytes to send in ICMP data field', int],
+        ['ttl', 't', 25, 'Set the IP Time to Live', int]]
 
 class EchoTest(nettest.NetTestCase):
     """
-    xxx fill me in
+    Basic ping test. This takes an input file containing one IP or hostname
+    per line.
     """
     name         = 'echo'
     author       = 'Isis Lovecruft <isis@torproject.org>'
     description  = 'A simple ping test to see if a host is reachable.'
-    version      = '0.0.2'
-    requiresRoot = True
+    version      = '0.0.4'
 
-    usageOptions    = UsageOptions
-    #requiredOptions = ['dst']
+    requiresRoot = True
+    usageOptions = EchoOptions
+    inputFile    = ['file', 'f', None, 'File of list of IPs to ping']
 
     def setUp(self, *a, **kw):
+        """
+        Send an ICMP-8 packet to a host IP, and process the response.
+
+        @param dst:
+            A single host to ping.
+        @param file:
+            A file of hosts to ping, one per line.
+        @param receive:
+            Whether or not to receive replies. Defaults to True.
+        """
         self.destinations = {}
 
         if self.localOptions:
             for key, value in self.localOptions.items():
-                log.debug("setting self.%s = %s" % (key, value))
                 setattr(self, key, value)
 
-        self.timeout *= 1000            ## convert to milliseconds
+        if hasattr(config.advanced, 'default_timeout'):
+            self.timeout = config.advanced.default_timeout
+        else:
+            self.timeout = 4
 
-        if not self.interface:
+        if config.advanced.interface:
+            self.interface = config.advanced.interface
+        else:
+            log.warn("No network interface specified in ooniprobe.conf!")
             try:
                 iface = txscapy.getDefaultIface()
-            except Exception, e:
-                log.msg("No network interface specified!")
-                log.err(e)
+            except Exception, ex:
+                log.err(ex.message)
             else:
-                log.msg("Using system default interface: %s" % iface)
                 self.interface = iface
+                log.msg("Using system default interface: %s" % iface)
 
-        if self.pcap:
-            try:
-                self.pcapfile = open(self.pcap, 'a+')
-            except:
-                log.msg("Unable to write to pcap file %s" % self.pcap)
-            else:
-                self.pcap = net.capturePacket(self.pcapfile)
+        self.payload = randomStr(self.payload_size)
 
-        if not self.dst:
-            if self.file:
-                self.dstProcessor(self.file)
-                for key, value in self.destinations.items():
-                    for label, data in value.items():
-                        if not 'ans' in data:
-                            self.dst = label
-        else:
-            self.addDest(self.dst)
-        log.debug("self.dst is now: %s" % self.dst)
-
-        log.debug("Initialization of %s test completed." % self.name)
-
-    def addDest(self, dest):
-        d = dest.strip()
-        self.destinations[d] = {'dst_ip': d}
-
-    def dstProcessor(self, inputfile):
-        from ipaddr import IPAddress
-
+    def inputProcessor(self, inputfile=None):
         if os.path.isfile(inputfile):
             with open(inputfile) as f:
                 for line in f.readlines():
                     if line.startswith('#'):
                         continue
-                    self.addDest(line)
+                    yield line.strip().rsplit(':', 1)[0] ## xxx not ipv6 safe
+
+    def icmp_constructor(self, dst=None):
+        """Construct a list of ICMP packets to send out."""
+        dst_ip = dst if dst is not None else self.input
+
+        packet_list = []
+        for x in xrange(self.count):
+            p = IP(dst=dst_ip, ttl=self.ttl)/ICMP(type=self.icmp_type)
+            p.add_payload(self.payload)
+            packet_list.append(p)
+        return packet_list
 
     def test_icmp(self):
-        def process_response(echo_reply, dest):
-           ans, unans = echo_reply
-           if ans:
-               log.msg("Recieved echo reply from %s: %s" % (dest, ans))
-           else:
-               log.msg("No reply was received from %s. Possible censorship event." % dest)
-               log.debug("Unanswered packets: %s" % unans)
-           self.report[dest] = echo_reply
+        """
+        Send the list of ICMP packets.
 
-        for label, data in self.destinations.items():
-            reply = sr1(IP(dst=lebal)/ICMP())
-            process = process_reponse(reply, label)
+        TODO: add end summary progress report for % answered, etc.
+        """
+        def nicely(thing):
+            """Log packets prettierly."""
+            return [x.summary() for x in thing]
 
-        #(ans, unans) = ping
-        #self.destinations[self.dst].update({'ans': ans,
-        #                                    'unans': unans,
-        #                                    'response_packet': ping})
-        #return ping
+        def process_answered(answered, sent):
+            """Callback function for sr()."""
 
-        #return reply
+            self.report[self.input] = {}
+            self.report[self.input]['answered'] = []
+            self.report[self.input]['unanswered'] = []
+            self.report[self.input]['sent'] = sent
+
+            if answered:
+                for resp in answered:
+                    log.msg("Received echo-reply:\n%s" % resp.summary())
+                    for snd in sent:
+                        if snd.dst == resp.src:
+                            answered.remove(resp)
+                            self.report[self.input]['answered'].append(resp)
+                        else:
+                            answered.remove(resp)
+                            self.report[self.input]['unanswered'].append(resp)
+
+
+        log.debug("Building packets...")
+
+        if self.input:
+            packets = self.icmp_constructor()
+            if self.dst:
+                packets.extend(self.icmp_constructor(self.dst))
+        else:
+            if self.dst:
+                packets = self.icmp_constructor(self.dst)
+
+        log.debug("Sending...")
+        d = defer.maybeDeferred(sr1, packets, iface=self.interface,
+                                timeout=self.timeout, nofilter=1)
+        d.addCallbacks(process_answered, log.exception, packets)
+
+        return d
