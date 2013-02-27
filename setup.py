7a55afa0399d2338f6ac5b8e2232e3645d694c06
Arturo Filastò <art@fuffa.org>
2013-02-27 15:59:01 +0100

    Update setup.py script and test that it works.

--- setup.py
+++ setup.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
+
+install_requires = [
+    'txsocksx>=0.0.2',
+    'scapy>=2.2.0',
+    'dnspython>=1.10.0',
+    'parsley>1.0',
+    'pypcap>=1.1.1'
+]
+
+dependency_links = [
+    'https://people.torproject.org/~ioerror/src/mirrors/ooniprobe',
+    'https://github.com/hellais/pypcap/archive/v1.1.1.tar.gz#egg=pypcap-1.1.1'
+]
+
+with open('requirements.txt') as f:
+    for line in f:
+        if line.startswith("#") or line.startswith('http'):
+            continue
+        install_requires.append(line)
 
 setup(
     name="ooni-probe",
-    version="0.0.8",
+    version="0.0.10",
     author="Arturo Filastò",
     author_email = "art@torproject.org",
     url="https://ooni.torproject.org/",
     package_dir={'ooni': 'ooni'},
     packages=['ooni', 'ooni.templates', 'ooni.utils'],
     scripts=["bin/ooniprobe"],
-    install_requires=open('requirements.txt').readlines(),
-    dependency_links=["https://hg.secdev.org/scapy/archive/tip.zip#egg=scapy"]
+    dependency_links=dependency_links,
+    install_requires=install_requires,
 )
