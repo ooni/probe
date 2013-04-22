3c86abb5d2a728604b97a33c3f989039231205b0
Arturo Filastò <art@fuffa.org>
2013-04-22 18:11:32 +0200

    Use absolute filepath instead of relative

--- ooni/tests/test_utils.py
+++ ooni/tests/test_utils.py
@@ -1,19 +1,20 @@
+import os
 import unittest
 from ooni.utils import pushFilenameStack
-
+basefilename = os.path.abspath('dummyfile')
 class TestUtils(unittest.TestCase):
     def test_pushFilenameStack(self):
-        f = open("dummyfile", "w+")
+        f = open(basefilename, "w+")
         f.write("0\n")
         f.close()
         for i in xrange(1, 5):
-            f = open("dummyfile.%s" % i, "w+")
+            f = open(basefilename+".%s" % i, "w+")
             f.write("%s\n" % i)
             f.close()
 
-        pushFilenameStack("dummyfile")
+        pushFilenameStack(basefilename)
         for i in xrange(1, 5):
-            f = open("dummyfile.%s" % i)
+            f = open(basefilename+".%s" % i)
             c = f.readlines()[0].strip()
             self.assertEqual(str(i-1), str(c))
             f.close()
