38188deb90379a44b6233e93ff8cf88e85ccf68e
Isis Lovecruft <isis@torproject.org>
2013-06-06 15:26:26 +0000

    Add versioneer to automatically generate oonib version numbers.
     * When a release is made, the tag MUST be prefixed correctly. To make a
       release, do:
    
       $ git checkout -b release-1.2.3
       [run some integration tests and other QA...]
       [write a CHANGELOG entry...]
       [you can make commits with minor fixes here too]
       $ git tag -a ooni-probe-1.2.3 -s
       $ git push
       $ git push --tags
       $ git checkout master
       $ git merge -S --no-ff release-1.2.3
       $ git checkout develop
       $ git merge -S --no-ff release-1.2.3
       $ git branch -d release-1.2.3
       $ python setup.py register sdist upload
    
       The version will automagically get bumped according to the number in the
       git tag. Additionally, if builds are created from a dirty repo, or with
       HEAD not at the commit of a signed tag, the last bit of the commit hash
       will be appended to the major version number so that we know precisely
       where the build came from.

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 
 from setuptools import setup
 
+import versioneer
+versioneer.versionfile_source = 'ooni/_version.py'
+versioneer.versionfile_build = 'ooni/_version.py'
+versioneer.tag_prefix = 'ooni-probe-'
+versioneer.parentdir_prefix = 'ooni-probe-'
+
 install_requires = [
     'txsocksx>=0.0.2',
     'scapy>=2.2.0',
@@ -22,13 +28,57 @@ with open('requirements.txt') as f:
 
 setup(
     name="ooni-probe",
-    version="0.0.11",
-    author="Arturo Filastò",
-    author_email = "art@torproject.org",
+    description="OONI network censorsorship detection client and network \
+test-writing framework",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="The Tor Project, Inc",
+    maintainer_email = "art@torproject.org",
     url="https://ooni.torproject.org/",
+    license="BSD License",
     package_dir={'ooni': 'ooni'},
     packages=['ooni', 'ooni.templates', 'ooni.utils'],
     scripts=["bin/ooniprobe"],
     dependency_links=dependency_links,
     install_requires=install_requires,
+    platforms="Linux, BSD, OSX, Windows",
+    ## gitweb.torproject.org doesn't have an easy way to point to the .zip
+    ## of the latest tag, so we'll have to fill this in later:
+    download_url="",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Framework :: Twisted",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: BSD License"
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: BSD :: BSD/OS",
+        "Operating System :: POSIX :: BSD :: FreeBSD",
+        "Operating System :: POSIX :: BSD :: NetBSD",
+        "Operating System :: POSIX :: BSD :: OpenBSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+        "Topic :: System :: Networking :: Monitoring",
+    ]
 )
