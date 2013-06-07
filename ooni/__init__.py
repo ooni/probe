05346a3a6df9a432cddd6d9f4eea7e260c57960f
Arturo Filastò <art@fuffa.org>
2013-06-08 01:29:07 +0200

    Fix breakage in setup.py

--- ooni/__init__.py
+++ ooni/__init__.py
@@ -1,13 +1,5 @@
 # -*- encoding: utf-8 -*-
 
-from . import config
-from . import kit
-from . import nettest
-from . import oonicli
-from . import reporter
-from . import templates
-from . import utils
-
 __author__ = "Arturo Filastò"
 __version__ = "0.0.12"
 
