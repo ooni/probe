b6545d318eeb838a44742c6ec2e7d986f0db5fa6
Arturo Filastò <art@fuffa.org>
2013-09-12 17:42:21 +0200

    bump to 1.0.0-rc2

--- ooni/__init__.py
+++ ooni/__init__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 __author__ = "Arturo Filastò"
-__version__ = "1.0.0-rc1"
+__version__ = "1.0.0-rc2"
 
 __all__ = ['config', 'inputunit', 'kit',
            'lib', 'nettest', 'oonicli', 'reporter',
