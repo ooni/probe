ea00095a440cbf84664dfec8880715d4615ead14
aagbsn <aagbsn@extc.org>
2013-11-22 18:10:06 +0000

    Check to see if a required option is a test helper we can look up

--- ooni/deck.py
+++ ooni/deck.py
@@ -118,15 +118,23 @@ class Deck(InputFile):
                 continue
             net_test_loader = NetTestLoader(test['options']['subargs'],
                     test_file=nettest_path)
+            self.insert(net_test_loader)
             #XXX: If the deck specifies the collector, we use the specified collector
             # And it should also specify the test helper address to use
             # net_test_loader.collector = test['options']['collector']
-            net_test_loader.checkOptions()
-            self.netTestLoaders.append(net_test_loader)
 
     def insert(self, net_test_loader):
         """ Add a NetTestLoader to this test deck """
-        net_test_loader.checkOptions()
+        try:
+            net_test_loader.checkOptions()
+        except e.MissingRequiredOption, option_name:
+            if not self.bouncer:
+                raise
+            for rth in net_test_loader.requiredTestHelpers:
+                if option_name.message == rth['option']:
+                    break
+            else:
+                raise
         self.netTestLoaders.append(net_test_loader)
 
     @defer.inlineCallbacks
