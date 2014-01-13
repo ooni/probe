e60ca5934b256f46ad47b3c2dce736bc075c9cf8
Arturo Filastò <art@fuffa.org>
2014-01-13 13:35:35 +0100

    Use the system Tor in the debian package

--- scripts/build-ooniprobe.sh
+++ scripts/build-ooniprobe.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 cd ../
+cat data/ooniprobe.conf.sample | sed s/'start_tor: true'/'start_tor: false'/ | sed s/'#socks_port: 8801'/'socks_port: 9050'/ > data/ooniprobe.conf.sample.new
+mv data/ooniprobe.conf.sample data/ooniprobe.conf.sample.bak 
+mv data/ooniprobe.conf.sample.new data/ooniprobe.conf.sample
 python setup.py sdist
 cd dist/
 py2dsc ooniprobe-*.tar.gz
@@ -8,3 +11,5 @@ cd deb_dist/ooniprobe-*
 rm -rf debian/
 cp -rf ../../../debian ./debian
 debuild
+cd ../../../
+mv data/ooniprobe.conf.sample.bak data/ooniprobe.conf.sample 
