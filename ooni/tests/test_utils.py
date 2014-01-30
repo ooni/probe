e8063623734901524c88544f883a6279302e2902
Arturo Filastò <art@fuffa.org>
2014-01-30 21:06:23 +0100

    Fix pushFilenameStack

--- ooni/tests/test_utils.py
+++ ooni/tests/test_utils.py
@@ -8,13 +8,13 @@ class TestUtils(unittest.TestCase):
         f = open(basefilename, "w+")
         f.write("0\n")
         f.close()
-        for i in xrange(1, 5):
+        for i in xrange(1, 20):
             f = open(basefilename+".%s" % i, "w+")
             f.write("%s\n" % i)
             f.close()
 
         pushFilenameStack(basefilename)
-        for i in xrange(1, 5):
+        for i in xrange(1, 20):
             f = open(basefilename+".%s" % i)
             c = f.readlines()[0].strip()
             self.assertEqual(str(i-1), str(c))
