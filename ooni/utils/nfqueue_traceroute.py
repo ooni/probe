2d23659c919ab643109f6c8e5e8fd4d39aa15753
David Stainton <dstainton415@gmail.com>
2014-02-04 16:07:44 +0000

    Some progress... still rough draft not working

--- ooni/utils/nfqueue_traceroute.py
+++ ooni/utils/nfqueue_traceroute.py
@@ -1,14 +1,60 @@
 #!/usr/bin/env python
 
 # Internal modules
-from ooni.utils.txscapy import ScapyProtocol
-from ooni.utils.nfqueue_reader import NFQueueReader
+from nfqueue_reader import NFQueueReader
 
 # External modules
-from scapy.all import IP, TCP
+import nfqueue
+from scapy.all import IP, IPerror, TCP, TCPerror, ICMP
+from twisted.internet import reactor
+
+class StreamTracker(object):
+
+    max_ttl = 30
+
+    def __init__(self):
+        self.last_ttl = 1
+
+        # XXX not yet used
+        # could be keyed with TCP sequence number + IP ID?
+        self.packets  = {}
+
+        # keyed with TCP sequence number
+        # value is TTL
+        self.ttls     = {}
+
+        self.packet_count = 0
+
+    def processPacket(self, queue_item, packet):
+
+        assert TCPerror not in packet
+        assert IPerror not in packet
+        assert ICMP not in packet
+
+        # keep track of mangled TTL per TCP sequence number
+        sequenceNum = packet[TCP].seq
+
+        if sequenceNum in self.ttls:
+            # we've already seen this sequence number
+            # don't mangle TCP retransmits
+            queue_item.set_verdict(nfqueue.NF_ACCEPT)
+            print "%s previously seen" % sequenceNum
+        else:
+            print "%s NOT previously seen" % sequenceNum
+            print "last_ttl %s" % self.last_ttl
+
+            self.last_ttl += 1
+
+            if self.last_ttl > self.max_ttl:
+                self.last_ttl = 1
+
+            packet.ttl     = self.last_ttl
+            del packet.chksum
+            self.ttls[sequenceNum] = self.last_ttl
+            queue_item.set_verdict_modified(nfqueue.NF_ACCEPT, str(packet), len(packet))
 
 
-class NFQueueTraceroute(ScapyProtocol):
+class NFQueueTraceroute(object):
 
     """
     MITM parasitic traceroute.
@@ -19,123 +65,92 @@ class NFQueueTraceroute(ScapyProtocol):
     ttl_min = 1
     ttl_max = 30 # XXX is this a good upper limit?
 
-    def __init__(self, nqueue=0, tcp_streams_to_mangle=[]):
+    def __init__(self, nqueue=0):
         """
-        nqueue indicates which NFQUEUE we should mangle packets from.
-        tcp_streams_to_mangle is a list of 4-tuples
-        (src_addr, src_port, dst_addr, dst_port)
-        which uniquely identify the TCP stream(s) to mangle.
+        nqueue indicates which NFQUEUE we should mangle packets from
+        AND
+        where we also receive ICMP packets (e.g. TTL expired)
         """
 
         self.nqueue = nqueue
 
-        # used to keep track of the last mangled TTL for each stream
-        # keyed with TCP 4-tuple
-        # value is last mangled TTL
-        self.stream_last_mangled_ttl = {}
-
-        # use this to determine hop count of ICMP errors
-        # by keeping track of the mangled TTL for corresponding
-        # TCP sequence number
-        # 
-        # outer dict keyed with TCP 4-tuple
-        # value is inner dict
-        # inner dict keyed with TCP sequence number
-        # inner dict value is mangled TTL
-        self.mangle_streams = {}
-
-        # Firstly, initialize all stream dicts to empty dict.
-        for stream_id in tcp_streams_to_mangle:
-            assert stream_id not in self.mangle_streams
-            assert len(stream_id) == 4
-            # XXX perhaps sanity check stream_id format?
-
-            self.mangle_streams[stream_id] = {}
-
-            # start off with mangled TTL of 1
-            self.stream_last_mangled_ttl[stream_id] = 1
+        # dict keyed with TCP 4-tuple
+        # value is a StreamTracker
+        self.mangled_streams = {}
 
         self.nfqueue_reader = NFQueueReader(self.handleNFQueuePacket, nqueue=self.nqueue)
 
-    def startTraceroute(self):
-        """To be called from the test class"""
+        # interesting things to report
+        self.report       = {}
+        self.error_report = {}
+
+    def start(self):
         reactor.addReader(self.nfqueue_reader)
 
-    def stopListening(self):
+    def stop(self):
         reactor.removeReader(self.nfqueue_reader)
-        self.factory.unRegisterProtocol(self)
+        print self.report
+        reactor.stop()
 
     def handleNFQueuePacket(self, queue_item):
-        ip_packet = IP(queue_item.get_data())
-        if self.isMangleStream(ip_packet):
-            self.manglePacket(queue_item, ip_packet)
+        packet = IP(queue_item.get_data())
 
-    def isMangleStream(self, ip_packet):
-        """
-        Return True or False depending on if
-        streamID is in our self.mangleStream dict
-        """
-        streamID  = ip_packet.src, ip_packet[TCP].sport, ip_packet.dst, ip_packet[TCP].dport
-        if streamID in self.mangleStream:
-            return True
+        if IPerror in packet:
+            print "ICMP"
+            self.handleICMP(queue_item, packet)
         else:
-            return False
+            print "IP"
+            stream_id = self.getStreamID(packet)
 
-    def manglePacket(self, queue_item, ip_packet):
-        """mangle TCP packet TTL"""
+            if stream_id not in self.mangled_streams:
+                print "new stream"
+                self.mangled_streams[stream_id] = StreamTracker()
 
-        # XXX later i need to develope more heuristics for deciding
-        # when to mangle packets...
-        # For now we mangle all packets that are keyed in self.mangle_streams
-        streamID = ip_packet.src, ip_packet[TCP].sport, ip_packet.dst, ip_packet[TCP].dport        
+            self.mangled_streams[stream_id].processPacket(queue_item, packet)
 
-        # keep track of mangled TTL per TCP sequence number
-        sequenceNum = ip_packet[TCP].seq
+    # given a scapy IP/TCP packet return a
+    # stream ID... a TCP 4-tuple
+    def getStreamID(self, packet):
+        if TCPerror in packet:
+            return packet.src, packet[TCPerror].sport, packet.dst, packet[TCPerror].dport
+        else:
+            return packet.src, packet[TCP].sport, packet.dst, packet[TCP].dport
 
-        # do not mangle the same packet twice to reduce
-        # impact on the mangled stream.
-        if sequenceNum not in self.mangle_streams[streamID]:
-            # keep track of last mangled TTL
-            self.stream_last_mangled_ttl[stream_id] += 1
-            new_ttl = self.stream_last_mangled_ttl[stream_id]
-            ip_packet.ttl = new_ttl
-            del ip_packet.chksum
-            self.mangle_streams[streamID][sequenceNum] = new_ttl
-            queue_item.set_verdict_modified(nfqueue.NF_ACCEPT, str(ip_packet), len(ip_packet))
+    def handleICMP(self, queue_item, packet):
+        """get TCP sequence number
+        and figure out the mangled TTL
+        value we used to produce this ICMP error...
+        """
 
-    def packetReceived(self, packet):
-        try:
-            packet[IP]
-        except IndexError:
-            return
+        print "handleICMP"
 
-        if isinstance(packet.getlayer(3), TCPerror):
-            self.received_packets.append(packet)
-            return
+        sequence_num = packet[ICMP].payload[TCPerror].seq
+        stream_id    = self.getStreamID(packet[ICMP].payload)
+
+        print "%s %s" % (sequence_num, stream_id)
+
+        if stream_id in self.mangled_streams:
+            mangled_ttl = self.mangled_streams[stream_id].ttls[sequence_num]
+            self.report[(stream_id, sequence_num)] = mangled_ttl
+        else:
+            self.error_report[(stream_id, sequence_num)] = packet
+
+        # XXX does it even matter if we accept or deny here?
+        queue_item.set_verdict(nfqueue.NF_DROP)
 
 
-class NFQueueTracerouteTest(scapyt.BaseScapyTest):
-# XXX should I use scapyt.BaseScapyTest or write something else?
 
-    name = "NFQueue TCP Traceroute Test"
-    description = "Performs a local MITM on specified TCP streams. Packets are intercepted and the TTLs are mangled before going out the network interface. We can thereby use this to cause any TCP stream (server or client) to perform a traceroute."
+# iptables -A OUTPUT -p tcp -m state --state RELATED,ESTABLISHED --dport 443 -j NFQUEUE --queue-num 0
+# iptables -A INPUT -p icmp -j NFQUEUE --queue-num 0
 
-    def setUp(self):
+def main():
 
-        # XXX fix this bullshit
-        streamIDs = [ (src_addr, src_port, dst_addr, dst_port) ]
+    nfqueue_traceroute = NFQueueTraceroute()
+    nfqueue_traceroute.start()
 
-        self.st = NFQueueTraceroute(nqueue=0, tcp_streams_to_mangle=streamIDs)
-        config.scapyFactory.registerProtocol(self.st)
+    reactor.callLater(30, lambda:nfqueue_traceroute.stop())
+    reactor.run()
+ 
+if __name__ == "__main__":
+    main()
 
-        self.report['NFQueueTracerouteTest'] = {}
-        
-
-    def test_nfqueue_traceroute(self):
-        # XXX should i be returning a deferred?
-        return self.st.startTraceroute()
-
-    def postProcessor(self, measurements):
-        # should be called after all deferreds have calledback
-        self.st.stopListening()
-        self.st.matchResponses()
