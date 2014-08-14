3c18956ca5480cf7d983de8d96fc0fb48bb34fa9
Arturo Filastò <art@fuffa.org>
2014-08-14 19:00:47 +0200

    Bump version to 1.1.0-rc1

--- ooni/__init__.py
+++ ooni/__init__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 __author__ = "Open Observatory of Network Interference"
-__version__ = "1.0.2"
+__version__ = "1.1.0-rc1"
 
 __all__ = ['config', 'inputunit', 'kit',
            'lib', 'nettest', 'oonicli', 'reporter',
