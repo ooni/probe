1883ede9a83e0006ef7eb9f0bbfeacb70659c0bc
Arturo Filastò <art@fuffa.org>
2014-08-19 11:38:25 +0200

    Update LICENSE and expand setup.py

--- setup.py
+++ setup.py
@@ -69,6 +69,22 @@ for root, dirs, file_names in os.walk('data/'):
         files.append(pj(root, file_name))
     data_files.append([pj(usr_share_path, 'ooni', root.replace('data/', '')), files])
 
+
+packages=[
+    'ooni',
+    'ooni.api',
+    'ooni.kit',
+    'ooni.nettests',
+    'ooni.nettests.manipulation',
+    'ooni.nettests.experimental',
+    'ooni.nettests.scanning',
+    'ooni.nettests.blocking',
+    'ooni.nettests.third_party',
+    'ooni.templates',
+    'ooni.tests',
+    'ooni.utils'
+]
+
 with open('requirements.txt') as f:
     for line in f:
         if line.startswith("#"):
@@ -78,21 +94,59 @@ with open('requirements.txt') as f:
             continue
         install_requires.append(line)
 
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('Changelog.rst') as f:
+    changelog = f.read()
+
 setup(
     name="ooniprobe",
     version=__version__,
     author=__author__,
-    author_email = "ooni-dev@torproject.org",
+    author_email="ooni-dev@torproject.org",
+    description="Network Interference detection tool.",
+    long_description=readme + '\n\n' + changelog,
+    license='BSD 2 clause',
     url="https://ooni.torproject.org/",
     package_dir={'ooni': 'ooni'},
     data_files=data_files,
-    packages=['ooni', 'ooni.api', 'ooni.kit',
-        'ooni.nettests', 'ooni.nettests.manipulation',
-        'ooni.nettests.experimental', 'ooni.nettests.scanning',
-        'ooni.nettests.blocking',
-        'ooni.nettests.third_party',
-        'ooni.templates', 'ooni.tests', 'ooni.utils'],
+    packages=packages,
     scripts=["bin/ooniprobe", "bin/oonireport"],
     dependency_links=dependency_links,
-    install_requires=install_requires
+    install_requires=install_requires,
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Framework :: Twisted",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: BSD License"
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: BSD :: BSD/OS",
+        "Operating System :: POSIX :: BSD :: FreeBSD",
+        "Operating System :: POSIX :: BSD :: NetBSD",
+        "Operating System :: POSIX :: BSD :: OpenBSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+        "Topic :: System :: Networking :: Monitoring",
+    )
 )
