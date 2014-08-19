3283a34cc487303cad2035f0257e88da04a38918
Arturo Filastò <art@fuffa.org>
2014-08-19 15:32:56 +0200

    update ooniprobe to 1.1.0

--- ooni/__init__.py
+++ ooni/__init__.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 __author__ = "Open Observatory of Network Interference"
-__version__ = "1.1.0-rc1"
+__version__ = "1.1.0"
 
 __all__ = ['config', 'inputunit', 'kit',
-           'lib', 'nettest', 'oonicli', 'reporter',
+           'lib', 'nettest', 'oonicli', 'report', 'reporter',
            'templates', 'utils']
