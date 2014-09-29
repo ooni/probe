404d499877c050f706ca0f713d1c8ef0e5a88889
Arturo Filastò <art@fuffa.org>
2014-09-29 14:10:49 +0200

    Set the datadirectory to be that of the repo.

--- ooni/tests/bases.py
+++ ooni/tests/bases.py
@@ -1,3 +1,5 @@
+import os
+
 from twisted.trial import unittest
 
 from ooni.settings import config
@@ -5,6 +7,8 @@ from ooni.settings import config
 
 class ConfigTestCase(unittest.TestCase):
     def setUp(self):
+        config.global_options['datadir'] = os.path.join(__file__, '..', '..', '..', 'data')
+        config.global_options['datadir'] = os.path.abspath(config.global_options['datadir'])
         config.initialize_ooni_home("ooni_home")
 
     def tearDown(self):
