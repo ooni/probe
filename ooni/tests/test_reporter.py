10740b3d141a7bce5854a19e97f3d31a629958f4
Arturo Filastò <art@fuffa.org>
2014-10-17 15:43:43 +0200

    testDone is no longer called so we can remove it from unittests.

--- ooni/tests/test_reporter.py
+++ ooni/tests/test_reporter.py
@@ -49,24 +49,14 @@ class TestYAMLReporter(unittest.TestCase):
             os.remove(self.filename)
 
     def test_write_report(self):
-        test = MockTest()
-
         y_reporter = YAMLReporter(test_details)
         y_reporter.createReport()
-        y_reporter.testDone(test, 'spam')
         with open(y_reporter.report_path) as f:
             self.filename = y_reporter.report_path
             report_entries = yaml.safe_load_all(f)
             # Check for keys in header
             entry = report_entries.next()
             assert all(x in entry for x in ['test_name', 'test_version'])
-
-            entry = report_entries.next()
-            # Check for first entry of report
-            assert all(x in entry
-                       for x in ['report_content', 'input',
-                                 'test_name', 'test_started',
-                                 'test_runtime'])
 
 
 class TestOONIBReporter(unittest.TestCase):
