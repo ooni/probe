38f10eccacd715c0081d81ea3047acd4e81fdb98
Daniel Ramsay <daniel@dretzq.org.uk>
2015-05-24 17:55:15 +0100

    remove iPhone useragent to prevent mobile versions being returned to client

--- ooni/utils/net.py
+++ ooni/utils/net.py
@@ -27,7 +27,6 @@ except ImportError:
 # agents with largest anonymity set.
 
 userAgents = ("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.7) Gecko/20091221 Firefox/3.5.7",
-              "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3 1 2 like Mac OS X; en-us)"
               "AppleWebKit/528.18 (KHTML, like Gecko) Mobile/7D11",
               "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6",
               "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6",
