525a8438bd601592c4f878ca5d42d3dab8943be0
srvetus <srvetus@users.noreply.github.com>
2015-11-19 14:16:42 +0100

    Test that specific Failures are caught before parent Failures

---
+++ ooni/tests/test_errors.py
@@ -0,0 +1,20 @@
+from twisted.trial import unittest
+
+import ooni.errors
+
+class TestErrors(unittest.TestCase):
+
+    def test_catch_child_failures_before_parent_failures(self):
+        """
+        Verify that more specific Failures are caught first by
+        handleAllFailures() and failureToString().
+
+        Fails if a subclass is listed after it's parent Failure.
+        """
+
+        # Check each Failure against all subsequent failures
+        for index, (failure, _) in enumerate(ooni.errors.known_failures):
+            for sub_failure, _ in ooni.errors.known_failures[index+1:]:
+
+                # Fail if subsequent Failure inherits from the current Failure
+                self.assertNotIsInstance(sub_failure(None), failure)
