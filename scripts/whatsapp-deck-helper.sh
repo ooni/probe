4a806709d4ddc215261b269ace41ac37e94e4033
anadahz <kojgelo@inbox.com>
2016-05-03 01:26:24 -0300

    Add Whatsapp deck and helper script for CIDR
    A Whatsapp ooni-probe deck with a shell script deck helper to grab and
    process Whatsapp's CIDR IPs. The Deck runs the following ooniprobe tests:
    - http_requests test on Whatsapp's HTTP/S URLs
    - dns_consistency test on Whatsapp's domain/subdomains
    - http_hosts tests on all oficial Whatapps CIDR IPs connecting on all
      know ports (80, 443, 5222, 5223, 5228, 5060, 5060, 8080).
    The deck helper script grabs the official CIDR IP list that Whatsapp
    uses to connect, extracts all the IPv4 addresses and converts the CIDR
    notations to IPs and finally splits them into list files per Whatsapp
    port.

---
+++ scripts/whatsapp-deck-helper.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+# This script grabs the CIDR IP list that Whatsapp uses to connect. Extracts
+# the IPv4 addresses and convert the CIDR notations to IPs. Finally splits the
+# IPs in separate list files per Whatsapp host port (IP:PORT) required for the
+# http_hosts ooni-probe test.
+# Required tools: prips, torsocks (optional but handy if you are blocked)
+
+# Official Whatsapp CIDR URL
+WHATSAPP_CIDR_URL="https://www.whatsapp.com/cidr.txt"
+# Known Whatsapp ports
+WHATSAPP_PORTS="80 443 5222 5223 5228 5060 5060 8080"
+# Known Whatsapp URLs
+WHATSAPP_URLS="
+www.whatsapp.com
+web.whatsapp.com
+www.whatsapp.com/cidr.txt
+"
+# Whatsapp URL list file
+URL_LIST="whatsappurl.list"
+
+if [ ! -f ${URL_LIST} ]; then
+	for u in ${WHATSAPP_URLS}; do
+		echo -e "http://${u}\nhttps://${u}" >> ${URL_LIST}
+	done
+fi
+
+# Check if prips and torsocks exists
+command -V prips torsocks
+
+torsocks wget -N ${WHATSAPP_CIDR_URL}
+sed '/^.*:.*$/d' cidr.txt > cidr-ipv4.txt
+
+while read l; do
+	prips $l; done <cidr-ipv4.txt > whatsapp-ipv4.list
+
+sed -i s/^.*m//g whatsapp-ipv4.list
+
+for p in ${WHATSAPP_PORTS}; do
+	sed s/$/:${p}/g whatsapp-ipv4.list > whatsapp-ipv4-${p}.list
+done
