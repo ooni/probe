a710472d5495de7e0e12b729d7ebb73234bd8fbc
Arturo Filastò <arturo@filasto.net>
2016-05-18 16:16:40 +0200

    Have a different version number for the resources than the ooniprobe version

--- ooni/__init__.py
+++ ooni/__init__.py
@@ -2,6 +2,9 @@
 
 __author__ = "Open Observatory of Network Interference"
 __version__ = "1.5.0-master"
+# This is the version number of resources to be downloaded
+# when a release is made it should be aligned to __version__
+__resources_version__ = "1.4.2"
 
 __all__ = ['config', 'inputunit', 'kit',
            'lib', 'nettest', 'oonicli', 'report', 'reporter',
--- ooni/resources/__init__.py
+++ ooni/resources/__init__.py
@@ -1,6 +1,7 @@
-from ooni import __version__ as ooniprobe_version
+from ooni import __resources_version__ as resources_version
 
 __version__ = "0.2.0"
 
 ooni_resources_url = ("https://github.com/TheTorProject/ooni-probe/releases"
-                      "/download/v{}/ooni-resources.tar.gz").format(ooniprobe_version)
+                      "/download/v{}/"
+                      "ooni-resources.tar.gz").format(resources_version)
