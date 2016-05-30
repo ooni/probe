8c4f794e8693d9953a8aa64b0af52ffc002add02
Arturo Filastò <arturo@filasto.net>
2016-05-30 18:40:25 +0200

    Set the version of master to be 1.5.1.dev1

--- ooni/__init__.py
+++ ooni/__init__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 __author__ = "Open Observatory of Network Interference"
-__version__ = "1.5.0"
+__version__ = "1.5.1.dev0"
 # This is the version number of resources to be downloaded
 # when a release is made it should be aligned to __version__
 __resources_version__ = "1.4.2"
