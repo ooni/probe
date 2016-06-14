90ec4fb643841b4a2c97fa2985e66d7138049467
Arturo Filastò <arturo@filasto.net>
2016-06-14 16:15:44 +0300

    Implementing bridge failover in ooniprobe
    We support failing over to obfs4 and meek when vanilla tor does not work.
    
    * This implements #538

--- ooni/oonicli.py
+++ ooni/oonicli.py
@@ -44,9 +44,9 @@ class Options(usage.Options):
         ["collector", "c", None, "Specify the address of the collector for "
                                  "test results. In most cases a user will "
                                  "prefer to specify a bouncer over this."],
-        ["bouncer", "b", None, "Specify the bouncer used to "
-                               "obtain the address of the "
-                               "collector and test helpers."],
+        ["bouncer", "b", CANONICAL_BOUNCER_ONION, "Specify the bouncer used to "
+                                            "obtain the address of the "
+                                            "collector and test helpers."],
         ["logfile", "l", None, "Write to this logs to this filename."],
         ["pcapfile", "O", None, "Write a PCAP of the ooniprobe session to "
                                 "this filename."],
