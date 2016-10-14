1ee23491f24f256263aa953e7aa63709f23169d8
Arturo Filastò <arturo@filasto.net>
2016-10-14 19:41:48 +0200

    Bump to version 2.0.0
    :tada: :octopus: :tada:

--- ooni/__init__.py
+++ ooni/__init__.py
@@ -1,8 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 __author__ = "Open Observatory of Network Interference"
-# XXX note this rc is not public just meant for internal testing
-__version__ = "2.0.0-rc4"
+__version__ = "2.0.0"
 
 __all__ = [
     'agent',
