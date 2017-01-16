fabce13a64f214610a2a1021fd105623f3c9862c
E.Iosifidis <iosifidise@gmail.com>
2017-01-16 19:06:17 +0200

    Update settings.py

--- ooni/settings.py
+++ ooni/settings.py
@@ -519,24 +519,24 @@ class OConfig(object):
                 except Exception:
                     incoherent.append('tor:socks_port')
 
-	    if self.tor.control_port is not None:
-       	        if isinstance(self.tor.control_port, int):
-	            control_port_ep = TCP4ClientEndpoint(reactor,
+            if self.tor.control_port is not None:
+                if isinstance(self.tor.control_port, int):
+                    control_port_ep = TCP4ClientEndpoint(reactor,
                                                          "localhost",
                                                          self.tor.control_port)
-		    try:
-		        yield connectProtocol(control_port_ep, ConnectAndCloseProtocol())
-		    except Exception:
-		        incoherent.append('tor:control_port')		
+                    try:
+                        yield connectProtocol(control_port_ep, ConnectAndCloseProtocol())
+                    except Exception:
+                        incoherent.append('tor:control_port')
                 else:
-		    conf_unix_socket_path = self.tor.control_port
-		    if conf_unix_socket_path.lstrip.startswith("unix:"):
-	                if os.path.exists(conf_unix_socket_path.lstrip("unix:")):
-			    unix_socket_path = conf_unix_socket_path.lstrip("unix:")
-			else:
-		 	    incoherent.append('tor:control_port')
+                    conf_unix_socket_path = self.tor.control_port
+                    if conf_unix_socket_path.lstrip.startswith("unix:"):
+                        if os.path.exists(conf_unix_socket_path.lstrip("unix:")):
+                            unix_socket_path = conf_unix_socket_path.lstrip("unix:")
+                        else:
+                            incoherent.append('tor:control_port')
                     else:
-		        incoherent.append('tor:control_port')
+                        incoherent.append('tor:control_port')
 
             self.log_incoherences(incoherent)
 
