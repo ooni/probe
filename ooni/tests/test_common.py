a3e0bb1b5ffe1b1b1dd359d8e537f01941d539bc
Arturo Filastò <arturo@filasto.net>
2017-01-31 15:03:35 +0000

    Fix bug in handling of request headers (#729)
    * Write a failing unittest demonstrating the bug
    
    * Fix bug that leads to Headers not being preserved across redirects
    
    * Fix backward compatibility with twisted < 15.0.0 when handling
      redirects
    
    * Skip the redirect test when the network is absent

--- ooni/tests/test_common.py
+++ ooni/tests/test_common.py
@@ -1,6 +1,13 @@
+import json
 from twisted.trial import unittest
+from twisted.internet import defer, reactor
+from twisted.web.client import readBody
+
+from . import is_internet_connected
+
 from ooni.common.http_utils import META_CHARSET_REGEXP
 from ooni.common.ip_utils import is_public_ipv4_address, is_private_ipv4_address
+from ooni.common.txextra import FixedRedirectAgent, TrueHeadersAgent, TrueHeaders
 
 class TestHTTPUtils(unittest.TestCase):
     def test_charset_detection(self):
@@ -31,3 +38,17 @@ class TestIPUtils(unittest.TestCase):
         self.assertFalse(is_private_ipv4_address('example.com'))
         self.assertTrue(is_private_ipv4_address('127.0.0.1'))
         self.assertTrue(is_private_ipv4_address('192.168.2.2'))
+
+class TestTxExtra(unittest.TestCase):
+    @defer.inlineCallbacks
+    def test_redirect_works(self):
+        if not is_internet_connected():
+            raise unittest.SkipTest("Internet connection missing")
+
+        agent = FixedRedirectAgent(TrueHeadersAgent(reactor))
+        headers = TrueHeaders({"Spam": ["ham"]})
+        url = "http://httpbin.org/absolute-redirect/3"
+        response = yield agent.request('GET', url, headers)
+        body = yield readBody(response)
+        j = json.loads(body)
+        self.assertEqual(j['headers']['Spam'], 'ham')
