ff8678565142978388e2019759d5717a90ba7982
Leonid Evdokimov <leon@darkk.net.ru>
2017-04-03 19:37:22 +0300

    Smear load generated by scheduler
    * Smear load generated by scheduler, see #630
    
    It runs ASAP after the installation. It survives reboot in predictable
    way: avoids double-run and does not skip scheduler cycle on restart.
    It supports non-24/7 operations as it targets running @daily task within
    2.4 hours window, so the probe that was down will likely run a task
    on boot and boot time is random enough for load smearing purposes.
    
    * Schedule decks right after initialisation, see #630
    
    That's required as refresh_deck_list() does not schedule anything if the
    `config` is not `is_initialized()`.
    
    Another possible way to implement this behavior is to postpone
    RefreshDeckList.task run checking config.is_initialized() in
    should_run().

--- ooni/ui/web/server.py
+++ ooni/ui/web/server.py
@@ -340,8 +340,8 @@ class WebUIAPI(object):
             except DeckNotFound:
                 raise WebUIError(404, 'Deck not found')
 
-        self.scheduler.refresh_deck_list()
         config.set_initialized()
+        self.scheduler.refresh_deck_list()
 
         self._is_initialized = True
 
